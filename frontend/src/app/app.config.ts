f1af2ec65f792914fb68192c1db158ea5dfd8266
Danrley Lima <danrleylac@gmail.com>
2025-03-30 00:35:51 -0300

    style: configurando o ng prime

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -2,6 +2,7 @@ import { ApplicationConfig } from '@angular/core';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeng/themes/aura';
+import Lara from '@primeng/themes/lara';
 
 import { provideRouter } from '@angular/router';
 
@@ -13,8 +14,12 @@ export const appConfig: ApplicationConfig = {
     provideAnimationsAsync(),
     providePrimeNG({
       theme: {
-        preset: Aura,
+        preset: Lara,
+        options: {
+          darkModeSelector: false || 'none'
+      }
       },
+      
     }),
   ],
 };
